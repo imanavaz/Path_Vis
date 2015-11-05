606126a581494276ffa866ffc4e498c9a7b2d2e2
imanavaz - Iman Avazpour <i_avazpour@yahoo.com>
2015-11-05 15:33:37 +1100

    worked with google maps, renames the map js file to gMap.

---
+++ Path_Visualization/gMapProcs.js
@@ -0,0 +1,32 @@
+﻿var gMapBase;
+var trajCanvasLayer;
+
+function initMap() {
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(-36.911568, 144.929646)
+    };
+    gMapBase = new google.maps.Map(document.getElementById('map-canvas'), 
+        mapOptions);
+
+    //map canvas layer
+    trajCanvasLayer = new CanvasLayer({
+        map: gMapBase,
+        update: trajCanvasUpdate()
+    })
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+//in case usign a canvas layer
+function trajCanvasUpdate() {
+    var scale = Math.pow(2, gMapBase.getZoom());
+    var offset = projection.fromLatLngToPoint(
+        trajCanvasLayer.getTopLeft());
+
+    context.scale(scale, scale);
+    context.traslate(-offset.x, -offset.y);
+}
+
+
