c64ec791086a40cef329b18a7918f2b49e0b100a
Iman <iman_avaz@hotmail.com>
2015-11-08 21:53:46 +1100

    Multiple waypoints problem fixed. Need to update the code to include steps.

--- Path_Visualization/gMapProcs.js
+++ Path_Visualization/gMapProcs.js
@@ -29,12 +29,117 @@ function processData(trajectoryList, directionsService, directionsDisplay) {
             if (d.Trajectory_ID == trajectoryList.value)
                 locations.push(d);
         });
+
+        //imported code
+        var batches = [];
+        var itemsPerBatch = 10; // google API max - 1 start, 1 stop, and 8 waypoints
+        var itemsCounter = 0;
+        var wayptsExist = locations.length > 0;
+
+        while (wayptsExist) {
+            var subBatch = [];
+            var subitemsCounter = 0;
+
+            for (var j = itemsCounter; j < locations.length; j++) {
+                subitemsCounter++;
+                subBatch.push({
+                    location: new window.google.maps.LatLng(locations[j].Latitude, locations[j].Longitude),
+                    stopover: true
+                });
+                if (subitemsCounter == itemsPerBatch)
+                    break;
+            }
+
+            itemsCounter += subitemsCounter;
+            batches.push(subBatch);
+            wayptsExist = itemsCounter < locations.length;
+            // If it runs again there are still points. Minus 1 before continuing to 
+            // start up with end of previous tour leg
+            itemsCounter--;
+        }
+
         if (locations.length > 0)
-            calculateAndDisplayRoute(directionsService, directionsDisplay, locations);
+            //calculateAndDisplayRoute(directionsService, directionsDisplay, locations);
+            calcRoute(batches, directionsService, directionsDisplay);
         else
             alert("No trajectories found!");
     });
 }
+
+
+function calcRoute (batches, directionsService, directionsDisplay) {
+    var combinedResults;
+    var unsortedResults = [{}]; // to hold the counter and the results themselves as they come back, to later sort
+    var directionsResultsReturned = 0;
+     
+    for (var k = 0; k < batches.length; k++) {
+        var lastIndex = batches[k].length - 1;
+        var start = batches[k][0].location;
+        var end = batches[k][lastIndex].location;
+         
+        // trim first and last entry from array
+        var waypts = [];
+        waypts = batches[k];
+        waypts.splice(0, 1);
+        waypts.splice(waypts.length - 1, 1);
+         
+        var request = {
+            origin : start,
+            destination : end,
+            waypoints : waypts,
+            travelMode: document.getElementById('mode').value
+        };
+        (function (kk) {
+            directionsService.route(request, function (result, status) {
+                if (status == window.google.maps.DirectionsStatus.OK) {
+                     
+                    var unsortedResult = {
+                        order : kk,
+                        result : result
+                    };
+                    unsortedResults.push(unsortedResult);
+                     
+                    directionsResultsReturned++;
+                     
+                    if (directionsResultsReturned == batches.length) // we've received all the results. put to map
+                    {
+                        // sort the returned values into their correct order
+                        unsortedResults.sort(function (a, b) {
+                            return parseFloat(a.order) - parseFloat(b.order);
+                        });
+                        var count = 0;
+                        for (var key in unsortedResults) {
+                            if (unsortedResults[key].result != null) {
+                                if (unsortedResults.hasOwnProperty(key)) {
+                                    if (count == 0) // first results. new up the combinedResults object
+                                        combinedResults = unsortedResults[key].result;
+                                    else {
+                                        // only building up legs, overview_path, and bounds in my consolidated object. This is not a complete
+                                        // directionResults object, but enough to draw a path on the map, which is all I need
+                                        combinedResults.routes[0].legs = combinedResults.routes[0].legs.concat(unsortedResults[key].result.routes[0].legs);
+                                        combinedResults.routes[0].overview_path = combinedResults.routes[0].overview_path.concat(unsortedResults[key].result.routes[0].overview_path);
+                                         
+                                        combinedResults.routes[0].bounds = combinedResults.routes[0].bounds.extend(unsortedResults[key].result.routes[0].bounds.getNorthEast());
+                                        combinedResults.routes[0].bounds = combinedResults.routes[0].bounds.extend(unsortedResults[key].result.routes[0].bounds.getSouthWest());
+                                    }
+                                    count++;
+                                }
+                            }
+                        }
+                        directionsDisplay.setDirections(combinedResults);
+                    }
+                }
+            });
+        })(k);
+    }
+}
+
+
+
+
+
+
+
 
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay, locations) {
